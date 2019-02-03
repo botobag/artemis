e08cd2d6932f44d4accde11163ae8eb762f3ec64
Zonr Chang <zonr@botobag.com>
2019-02-03 23:33:25 +0800

    [core] Add Inspect to make value printing consistent with graphql-js.
    Ref: #45.

---
+++ graphql/inspect.go
@@ -0,0 +1,192 @@
+/**
+ * Copyright (c) 2019, The Artemis Authors.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package graphql
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"runtime"
+
+	"github.com/botobag/artemis/internal/util"
+
+	"github.com/json-iterator/go"
+)
+
+// ValueWithCustomInspect provides custom inspect function to serialize value in Inspect.
+type ValueWithCustomInspect interface {
+	Inspect(out io.Writer) error
+}
+
+// InspectToBuf prints Go values v to the given buf in the same format as graphql-js's inspect
+// function.  The implementation matches
+// https://github.com/graphql/graphql-js/blob/4cdc8e2/src/jsutils/inspect.js.
+func InspectToBuf(v interface{}, out io.Writer) error {
+	if v, ok := v.(ValueWithCustomInspect); ok {
+		return v.Inspect(out)
+	}
+
+	value := reflect.ValueOf(v)
+	switch value.Kind() {
+	case reflect.String:
+		// graphql-js: JSON.stringify(value)
+		stream := jsoniter.NewStream(jsoniter.ConfigDefault, out, 0)
+		stream.WriteString(v.(string))
+		if err := stream.Flush(); err != nil {
+			return err
+		}
+
+	case reflect.Func:
+		f := runtime.FuncForPC(value.Pointer())
+		if _, err := out.Write([]byte{'[', 'f', 'u', 'n', 'c', 't', 'i', 'o', 'n', ' '}); err != nil {
+			return err
+		}
+		if _, err := out.Write([]byte(f.Name())); err != nil {
+			return err
+		}
+		if _, err := out.Write([]byte{']'}); err != nil {
+			return err
+		}
+
+	case reflect.Array, reflect.Slice:
+		if _, err := out.Write([]byte{'['}); err != nil {
+			return err
+		}
+		size := value.Len()
+		if size > 0 {
+			if err := InspectToBuf(value.Index(0).Interface(), out); err != nil {
+				return err
+			}
+			for i := 1; i < size; i++ {
+				if _, err := out.Write([]byte{',', ' '}); err != nil {
+					return err
+				}
+				if err := InspectToBuf(value.Index(i).Interface(), out); err != nil {
+					return err
+				}
+			}
+		}
+		if _, err := out.Write([]byte{']'}); err != nil {
+			return err
+		}
+
+	case reflect.Map:
+		size := value.Len()
+		if size == 0 {
+			if _, err := out.Write([]byte{'{', '}'}); err != nil {
+				return err
+			}
+		} else {
+			if _, err := out.Write([]byte{'{', ' '}); err != nil {
+				return err
+			}
+
+			keys := value.MapKeys()
+			for i, key := range keys {
+				// Write key.
+				if err := InspectToBuf(key.Interface(), out); err != nil {
+					return err
+				}
+				if _, err := out.Write([]byte{':', ' '}); err != nil {
+					return err
+				}
+				// Write value.
+				if err := InspectToBuf(value.MapIndex(key).Interface(), out); err != nil {
+					return err
+				}
+				if i != len(keys)-1 {
+					if _, err := out.Write([]byte{',', ' '}); err != nil {
+						return err
+					}
+				}
+			}
+
+			if _, err := out.Write([]byte{' ', '}'}); err != nil {
+				return err
+			}
+		}
+
+	case reflect.Struct:
+		typ := value.Type()
+		numFields := typ.NumField()
+		if numFields == 0 {
+			if _, err := out.Write([]byte{'{', '}'}); err != nil {
+				return err
+			}
+		} else {
+			if _, err := out.Write([]byte{'{', ' '}); err != nil {
+				return err
+			}
+
+			for i := 0; i < numFields; i++ {
+				field := typ.Field(i)
+				// Write field name.
+				if _, err := out.Write([]byte(field.Name)); err != nil {
+					return err
+				}
+
+				if _, err := out.Write([]byte{':', ' '}); err != nil {
+					return err
+				}
+
+				// Write value.
+				if err := InspectToBuf(value.Field(i).Interface(), out); err != nil {
+					return err
+				}
+
+				if i != numFields-1 {
+					if _, err := out.Write([]byte{',', ' '}); err != nil {
+						return err
+					}
+				}
+			}
+
+			if _, err := out.Write([]byte{' ', '}'}); err != nil {
+				return err
+			}
+		}
+
+	case reflect.Ptr:
+		elem := value.Elem()
+		if !elem.IsValid() {
+			_, err := out.Write([]byte{'n', 'u', 'l', 'l'})
+			return err
+		}
+		return InspectToBuf(elem.Interface(), out)
+
+	case reflect.Invalid:
+		if _, err := out.Write([]byte{'n', 'u', 'l', 'l'}); err != nil {
+			return err
+		}
+
+	default:
+		if _, err := fmt.Fprint(out, v); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Inspect calls InspectOrErr but panics on error.
+func Inspect(v interface{}) string {
+	var buf util.StringBuilder
+	if err := InspectToBuf(v, &buf); err != nil {
+		panic(fmt.Sprintf("inspect %+v with error: %s", v, err))
+	}
+	return buf.String()
+}
---
+++ graphql/inspect_test.go
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) 2019, The Artemis Authors.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package graphql_test
+
+import (
+	"errors"
+	"io"
+	"math"
+
+	"github.com/botobag/artemis/graphql"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func testFunc() {}
+
+type objectWithCustomInspect struct {
+	str string
+}
+
+func (o objectWithCustomInspect) Inspect(out io.Writer) error {
+	s := o.str
+	if len(s) == 0 {
+		s = "<custom inspect>"
+	}
+	if _, err := out.Write([]byte(s)); err != nil {
+		return err
+	}
+	return nil
+}
+
+type objectWithErrCustomInspect struct {
+	err error
+}
+
+func (o objectWithErrCustomInspect) Inspect(out io.Writer) error {
+	return o.err
+}
+
+// graphql-js/src/jsutils/__tests__/inspect-test.js
+var _ = Describe("Inspect", func() {
+	It("null", func() {
+		Expect(graphql.Inspect(nil)).Should(Equal("null"))
+	})
+
+	It("boolean", func() {
+		Expect(graphql.Inspect(true)).Should(Equal("true"))
+		Expect(graphql.Inspect(false)).Should(Equal("false"))
+	})
+
+	It("string", func() {
+		Expect(graphql.Inspect("")).Should(Equal(`""`))
+		Expect(graphql.Inspect("abc")).Should(Equal(`"abc"`))
+		Expect(graphql.Inspect("\"")).Should(Equal(`"\""`))
+	})
+
+	It("number", func() {
+		Expect(graphql.Inspect(0)).Should(Equal(`0`))
+		Expect(graphql.Inspect(3.14)).Should(Equal(`3.14`))
+		Expect(graphql.Inspect(math.NaN())).Should(Equal(`NaN`))
+		// The following are different than graphql-js which are printed as Infinity and -Infinity,
+		// repectively.
+		Expect(graphql.Inspect(math.Inf(+1))).Should(Equal(`+Inf`))
+		Expect(graphql.Inspect(math.Inf(-1))).Should(Equal(`-Inf`))
+	})
+
+	It("function", func() {
+		Expect(graphql.Inspect(func() int { return 0 })).Should(MatchRegexp(`^\[function github.com/botobag/artemis/graphql_test\.glob.+\]$`))
+		Expect(graphql.Inspect(testFunc)).Should(MatchRegexp(`^\[function github.com/botobag/artemis/graphql_test\.testFunc]$`))
+	})
+
+	It("array", func() {
+		Expect(graphql.Inspect([]interface{}(nil))).Should(Equal(`[]`))
+		Expect(graphql.Inspect([]interface{}{})).Should(Equal(`[]`))
+		Expect(graphql.Inspect([]interface{}{nil})).Should(Equal(`[null]`))
+		Expect(graphql.Inspect([]interface{}{1, math.NaN()})).Should(Equal(`[1, NaN]`))
+		Expect(graphql.Inspect([]interface{}{
+			[]string{"a", "b"},
+			"c",
+		})).Should(Equal(`[["a", "b"], "c"]`))
+	})
+
+	It("object", func() {
+		Expect(graphql.Inspect(struct{}{})).Should(Equal(`{}`))
+		Expect(graphql.Inspect((*struct{})(nil))).Should(Equal(`null`))
+
+		Expect(graphql.Inspect(struct {
+			A int
+		}{
+			A: 1,
+		})).Should(Equal(`{ A: 1 }`))
+
+		Expect(graphql.Inspect(struct {
+			A int
+			B int
+		}{
+			A: 1,
+			B: 2,
+		})).Should(Equal(`{ A: 1, B: 2 }`))
+
+		Expect(graphql.Inspect(struct {
+			Array []interface{}
+		}{
+			Array: []interface{}{nil, 0},
+		})).Should(Equal(`{ Array: [null, 0] }`))
+
+		Expect(graphql.Inspect(struct {
+			A bool
+			B interface{}
+		}{
+			A: true,
+			B: nil,
+		})).Should(Equal(`{ A: true, B: null }`))
+	})
+
+	It("map", func() {
+		Expect(graphql.Inspect(map[string]interface{}(nil))).Should(Equal(`{}`))
+
+		Expect(graphql.Inspect(map[string]interface{}{
+			"a": 1,
+		})).Should(Equal(`{ "a": 1 }`))
+
+		Expect(graphql.Inspect(map[string]interface{}{
+			"a": 1,
+			"b": 2,
+		})).Should(Or(
+			Equal(`{ "a": 1, "b": 2 }`),
+			Equal(`{ "b": 2, "a": 1 }`),
+		))
+
+		Expect(graphql.Inspect(map[string]interface{}{
+			"array": []interface{}{nil, 0},
+		})).Should(Equal(`{ "array": [null, 0] }`))
+
+		Expect(graphql.Inspect(map[string]interface{}{
+			"a": true,
+			"b": nil,
+		})).Should(Or(
+			Equal(`{ "a": true, "b": null }`),
+			Equal(`{ "b": null, "a": true }`),
+		))
+	})
+
+	It("pointer", func() {
+		s := "Hello, World!"
+		Expect(graphql.Inspect(&s)).Should(Equal(`"Hello, World!"`))
+	})
+
+	It("custom inspect", func() {
+		Expect(graphql.Inspect(objectWithCustomInspect{})).Should(Equal(`<custom inspect>`))
+	})
+
+	It("custom inspect function that uses this", func() {
+		Expect(graphql.Inspect(objectWithCustomInspect{"Hello World!"})).Should(Equal(`Hello World!`))
+	})
+
+	It("custom inspect function that returns error", func() {
+		Expect(func() {
+			graphql.Inspect(objectWithErrCustomInspect{errors.New("error")})
+		}).Should(Panic())
+	})
+})
